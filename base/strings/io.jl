9e14b924344c47e5aa78929f2398c5e615acf753
Jakob Nybo Nissen <jakobnybonissen@gmail.com>
2024-05-10 18:45:54 +0200

    Use (unsafe_)takestring! throughout Base

--- base/strings/io.jl
+++ base/strings/io.jl
@@ -25,7 +25,7 @@ julia> io = IOBuffer();
 
 julia> print(io, "Hello", ' ', :World!)
 
-julia> String(take!(io))
+julia> takestring!(io)
 "Hello World!"
 ```
 """
@@ -68,7 +68,7 @@ julia> io = IOBuffer();
 
 julia> println(io, "Hello", ',', " world.")
 
-julia> String(take!(io))
+julia> takestring!(io)
 "Hello, world.\\n"
 ```
 """
@@ -298,10 +298,10 @@ Create a read-only `IOBuffer` on the data underlying the given string.
 ```jldoctest
 julia> io = IOBuffer("Haho");
 
-julia> String(take!(io))
+julia> takestring!(io)
 "Haho"
 
-julia> String(take!(io))
+julia> takestring!(io)
 "Haho"
 ```
 """
@@ -777,7 +777,7 @@ function unindent(str::AbstractString, indent::Int; tabwidth=8)
             print(buf, ' ')
         end
     end
-    String(take!(buf))
+    unsafe_takestring!(buf)
 end
 
 function String(a::AbstractVector{Char})
